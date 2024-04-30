fe7c913ea7f26e2f378082366fe386f297ba710d
YaoTiancheng <YaoTiancheng@outlook.com>
2024-04-30 11:39:38 +0800

    Rewrote the lighting demo with CDemoApp.

--- Lighting/Main.cpp
+++ Lighting/Main.cpp
@@ -1,20 +1,37 @@
 
 #include "PCH.h"
 #include "Rasterizer.h"
+#include "DemoApp.h"
 #include "Scene.h"
 #include "SceneLoader.h"
 #include "SceneRendering.h"
 
-using namespace Microsoft::WRL;
 using namespace DirectX;
 
-static const wchar_t* s_WindowClassName = L"RasterizerWindow";
+class CDemoApp_Lighting : public CDemoApp
+{
+    using CDemoApp::CDemoApp;
 
-static uint32_t s_LightOrbitMode = 0;
-static Rasterizer::ELightingModel s_LightingModel = Rasterizer::ELightingModel::eBlinnPhong;
-static Rasterizer::ELightType s_LightType = Rasterizer::ELightType::eDirectional;
+    virtual bool OnInit() override;
+    virtual void OnDestroy() override;
+    virtual void OnUpdate() override;
+    virtual bool OnWndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam ) override;
 
-static LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+    bool CreateRenderData( uint32_t width, uint32_t height );
+    void DestroyRenderData();
+
+    uint32_t m_LightOrbitMode = 0;
+    Rasterizer::ELightingModel m_LightingModel = Rasterizer::ELightingModel::eBlinnPhong;
+    Rasterizer::ELightType m_LightType = Rasterizer::ELightType::eDirectional;
+    float m_LightOrbitAngle = 0.f;
+
+    Rasterizer::SImage m_RenderTarget;
+    Rasterizer::SImage m_DepthTarget;
+    CScene m_Scene;
+    std::vector<SMeshDrawCommand> m_MeshDrawCommands;
+};
+
+bool CDemoApp_Lighting::OnWndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
 {
     switch ( message )
     {
@@ -23,103 +40,59 @@ static LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM 
         { 
             if ( wParam == 'O' )
             {
-                s_LightOrbitMode ^= 1u;
+                m_LightOrbitMode ^= 1u;
             }
             else if ( wParam == 'L' )
             {
-                s_LightType = s_LightType == Rasterizer::ELightType::eDirectional ? Rasterizer::ELightType::ePoint : Rasterizer::ELightType::eDirectional;
+                m_LightType = m_LightType == Rasterizer::ELightType::eDirectional ? Rasterizer::ELightType::ePoint : Rasterizer::ELightType::eDirectional;
             }
             else if ( wParam == 'M' )
             {
-                s_LightingModel = (Rasterizer::ELightingModel)( ( (uint32_t)s_LightingModel + 1 ) % (uint32_t)Rasterizer::ELightingModel::eCount );
+                m_LightingModel = (Rasterizer::ELightingModel)( ( (uint32_t)m_LightingModel + 1 ) % (uint32_t)Rasterizer::ELightingModel::eCount );
             }
         }
         break;
-    case WM_DESTROY:
-        PostQuitMessage( 0 );
-        break;
-    default:
-        return DefWindowProc( hWnd, message, wParam, lParam );
     }
-    return 0;
+    return false;
 }
 
-static HWND CreateAppWindow( HINSTANCE hInstance, uint32_t width, uint32_t height )
+bool CDemoApp_Lighting::CreateRenderData( uint32_t width, uint32_t height )
 {
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof( WNDCLASSEX );
-
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = NULL;
-    wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
-    wcex.hbrBackground = (HBRUSH)( COLOR_WINDOW + 1 );
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = s_WindowClassName;
-    wcex.hIconSm = NULL;
-
-    if ( !RegisterClassExW( &wcex ) )
-    {
-        return NULL;
-    }
-
-    const DWORD windowStyle = WS_OVERLAPPEDWINDOW & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX;
-
-    RECT rect;
-    rect.left = 0;
-    rect.top = 0;
-    rect.right = width;
-    rect.bottom = height;
-    AdjustWindowRect( &rect, windowStyle, FALSE );
-
-    HWND hWnd = CreateWindowW( s_WindowClassName, L"Lighting", windowStyle,
-        CW_USEDEFAULT, 0, rect.right - rect.left, rect.bottom - rect.top, nullptr, nullptr, hInstance, nullptr );
-
-    return hWnd;
-}
-
-static bool CreateRenderData( uint32_t width, uint32_t height, Rasterizer::SImage* renderTarget, Rasterizer::SImage* depthTarget,
-    CScene* scene, std::vector<SMeshDrawCommand>* commands )
-{
-    if ( !LoadSceneFronGLTFFile( "Resources/Teapot.glb", scene ) )
+    if ( !LoadSceneFronGLTFFile( "Resources/Teapot.glb", &m_Scene ) )
     {
         return false;
     }
 
     // The teapot.obj is in right hand coordinate, convert it to left hand coordinate
-    scene->FlipCoordinateHandness();
+    m_Scene.FlipCoordinateHandness();
 
-    GenerateMeshDrawCommands( *scene, commands );
+    GenerateMeshDrawCommands( m_Scene, &m_MeshDrawCommands );
 
-    renderTarget->m_Width = width;
-    renderTarget->m_Height = height;
-    renderTarget->m_Bits = (uint8_t*)malloc( width * height * 4 );
+    m_RenderTarget.m_Width = width;
+    m_RenderTarget.m_Height = height;
+    m_RenderTarget.m_Bits = (uint8_t*)malloc( width * height * 4 );
 
-    depthTarget->m_Width = width;
-    depthTarget->m_Height = height;
-    depthTarget->m_Bits = (uint8_t*)malloc( width * height * 4 );
+    m_DepthTarget.m_Width = width;
+    m_DepthTarget.m_Height = height;
+    m_DepthTarget.m_Bits = (uint8_t*)malloc( width * height * 4 );
 
     return true;
 }
 
-static void DestroyRenderData( Rasterizer::SImage* renderTarget, Rasterizer::SImage* depthTarget, CScene* scene )
+void CDemoApp_Lighting::DestroyRenderData()
 {
-    scene->FreeAll();
-    free( renderTarget->m_Bits );
-    free( depthTarget->m_Bits );
+    m_Scene.FreeAll();
+    free( m_RenderTarget.m_Bits );
+    free( m_DepthTarget.m_Bits );
 }
 
-static void RenderImage( ID2D1Bitmap* d2dBitmap, Rasterizer::SImage& renderTarget, Rasterizer::SImage& depthTarget, const std::vector<SMeshDrawCommand>& commands, float aspectRatio, float& lightOrbitAngle )
+void CDemoApp_Lighting::OnUpdate()
 {
-    lightOrbitAngle += XMConvertToRadians( 0.5f );
+    m_LightOrbitAngle += XMConvertToRadians( 0.5f );
 
-    ZeroMemory( renderTarget.m_Bits, renderTarget.m_Width * renderTarget.m_Height * 4 );
-    float* depthBit = (float*)depthTarget.m_Bits;
-    for ( uint32_t i = 0; i < depthTarget.m_Width * depthTarget.m_Height; ++i )
+    ZeroMemory( m_RenderTarget.m_Bits, m_RenderTarget.m_Width * m_RenderTarget.m_Height * 4 );
+    float* depthBit = (float*)m_DepthTarget.m_Bits;
+    for ( uint32_t i = 0; i < m_DepthTarget.m_Width * m_DepthTarget.m_Height; ++i )
     {
         *depthBit = 1.f;
         ++depthBit;
@@ -127,6 +100,7 @@ static void RenderImage( ID2D1Bitmap* d2dBitmap, Rasterizer::SImage& renderTarge
 
     Rasterizer::SMatrix matrix;
 
+    const float aspectRatio = (float)m_RenderTarget.m_Width / m_RenderTarget.m_Height;
     const XMMATRIX projectionMatrix = XMMatrixPerspectiveFovLH( XMConvertToRadians( 40.0f ), aspectRatio, 2.f, 1000.f );
     XMStoreFloat4x4A( (XMFLOAT4X4A*)&matrix, projectionMatrix );
     Rasterizer::SetProjectionTransform( matrix );
@@ -141,10 +115,10 @@ static void RenderImage( ID2D1Bitmap* d2dBitmap, Rasterizer::SImage& renderTarge
     light.m_Diffuse = Rasterizer::SVector3( 1.f, 1.f, 1.f );
     light.m_Specular = Rasterizer::SVector3( 1.f, 1.f, 1.f );
     light.m_Ambient = Rasterizer::SVector3( 0.05f, 0.06f, 0.05f );
-    XMMATRIX rotationMatrix = s_LightOrbitMode == 0 ? XMMatrixRotationRollPitchYaw( 0.f, lightOrbitAngle, 0.f ) : XMMatrixRotationRollPitchYaw( lightOrbitAngle, 0.f, 0.f );
+    XMMATRIX rotationMatrix = m_LightOrbitMode == 0 ? XMMatrixRotationRollPitchYaw( 0.f, m_LightOrbitAngle, 0.f ) : XMMatrixRotationRollPitchYaw( m_LightOrbitAngle, 0.f, 0.f );
     XMMATRIX lightWorldViewMatrix = XMMatrixMultiply( rotationMatrix, viewMatrix );
-    XMVECTOR lightPosition = s_LightOrbitMode == 0 ? XMVectorSet( -3.1f, 0.f, 0.f, 0.f ) : XMVectorSet( 0.f, 3.1f, 0.f, 0.f );
-    lightPosition = s_LightType == Rasterizer::ELightType::eDirectional ? 
+    XMVECTOR lightPosition = m_LightOrbitMode == 0 ? XMVectorSet( -3.1f, 0.f, 0.f, 0.f ) : XMVectorSet( 0.f, 3.1f, 0.f, 0.f );
+    lightPosition = m_LightType == Rasterizer::ELightType::eDirectional ? 
         XMVector3Normalize( XMVector3TransformNormal( lightPosition, lightWorldViewMatrix ) ) :
         XMVector3Transform( lightPosition, lightWorldViewMatrix );
     XMStoreFloat3( (XMFLOAT3*)light.m_Position.m_Data, lightPosition );
@@ -157,11 +131,11 @@ static void RenderImage( ID2D1Bitmap* d2dBitmap, Rasterizer::SImage& renderTarge
     Rasterizer::SetMaterial( material );
 
     Rasterizer::SPipelineState pipelineState;
-    pipelineState.m_LightingModel = s_LightingModel;
-    pipelineState.m_LightType = s_LightType;
+    pipelineState.m_LightingModel = m_LightingModel;
+    pipelineState.m_LightType = m_LightType;
     Rasterizer::SetPipelineState( pipelineState );
 
-    for ( const SMeshDrawCommand& command : commands )
+    for ( const SMeshDrawCommand& command : m_MeshDrawCommands )
     {
         Rasterizer::SetPositionStream( command.m_PositionStream );
         Rasterizer::SetNormalStream( command.m_NormalStream );
@@ -185,58 +159,18 @@ static void RenderImage( ID2D1Bitmap* d2dBitmap, Rasterizer::SImage& renderTarge
             Rasterizer::Draw( 0, command.m_PrimitiveCount );
         }
     }
+
+    CopyToSwapChain( m_RenderTarget );
 }
 
-int APIENTRY wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+bool CDemoApp_Lighting::OnInit()
 {
-    UNREFERENCED_PARAMETER( hPrevInstance );
-    UNREFERENCED_PARAMETER( lpCmdLine );
+    uint32_t width, height;
+    GetSwapChainSize( &width, &height );
 
-    const uint32_t width = 800;
-    const uint32_t height = 600;
-    const float aspectRatio = (float)width / height;
-
-    HWND hWnd = CreateAppWindow( hInstance, width, height );
-    if ( !hWnd )
+    if ( !CreateRenderData( width, height ) )
     {
-        return 0;
-    }
-
-    ComPtr<ID2D1Factory> d2dFactory;
-    ComPtr<ID2D1HwndRenderTarget> d2dRenderTarget;
-    ComPtr<ID2D1Bitmap> d2dBitmap;
-
-    HRESULT hr = S_OK;
-    hr = D2D1CreateFactory( D2D1_FACTORY_TYPE_SINGLE_THREADED, d2dFactory.GetAddressOf() );
-    if ( FAILED( hr ) )
-    {
-        return 0;
-    }
-
-    const D2D1_SIZE_U d2dSize = { width, height };
-
-    hr = d2dFactory->CreateHwndRenderTarget( D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties( hWnd, d2dSize ), d2dRenderTarget.GetAddressOf() );
-    if ( FAILED( hr ) )
-    {
-        return 0;
-    }
-
-    hr = d2dRenderTarget->CreateBitmap( d2dSize, D2D1::BitmapProperties( D2D1::PixelFormat( DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE ) ), d2dBitmap.GetAddressOf() );
-    if ( FAILED( hr ) )
-    {
-        return 0;
-    }
-
-    ShowWindow( hWnd, nCmdShow );
-    UpdateWindow( hWnd );
-
-    Rasterizer::SImage renderTarget;
-    Rasterizer::SImage depthTarget;
-    CScene scene;
-    std::vector<SMeshDrawCommand> meshDrawCommands;
-    if ( !CreateRenderData( width, height, &renderTarget, &depthTarget, &scene, &meshDrawCommands ) )
-    {
-        return 0;
+        return false;
     }
 
     Rasterizer::SViewport viewport;
@@ -246,51 +180,31 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstanc
     viewport.m_Height = height;
 
     Rasterizer::Initialize();
-    Rasterizer::SetRenderTarget( renderTarget );
-    Rasterizer::SetDepthTarget( depthTarget );
+    Rasterizer::SetRenderTarget( m_RenderTarget );
+    Rasterizer::SetDepthTarget( m_DepthTarget );
     Rasterizer::SetViewport( viewport );
 
-    float lightOrbitAngle = 0.f;
+    return true;
+}
 
-    MSG msg;
-    bool looping = true;
-    while ( looping )
-    { 
-        while ( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )
-        {
-            if ( msg.message == WM_QUIT )
-            {
-                looping = false;
-                break;
-            }
-            
-            TranslateMessage( &msg );
-            DispatchMessage( &msg );
-        }
+void CDemoApp_Lighting::OnDestroy()
+{
+    DestroyRenderData();
+}
 
-        RenderImage( d2dBitmap.Get(), renderTarget, depthTarget, meshDrawCommands, aspectRatio, lightOrbitAngle );
+int APIENTRY wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+{
+    UNREFERENCED_PARAMETER( hPrevInstance );
+    UNREFERENCED_PARAMETER( lpCmdLine );
 
-        D2D1_RECT_U d2dRect = { 0, 0, width, height };
-        HRESULT hr = d2dBitmap->CopyFromMemory( &d2dRect, renderTarget.m_Bits, width * 4 );
-        if ( FAILED( hr ) )
-        {
-            looping = false;
-        }
-
-        d2dRenderTarget->BeginDraw();
-        d2dRenderTarget->DrawBitmap( d2dBitmap.Get() );
-        hr = d2dRenderTarget->EndDraw();
-        if ( FAILED( hr ) )
-        {
-            looping = false;
-        }
+    CDemoApp_Lighting demoApp( L"Lighting", hInstance, 800, 600 );
+    int returnCode = 0;
+    if ( demoApp.Initialize() )
+    {
+        returnCode = demoApp.Execute();
     }
-
-    DestroyRenderData( &renderTarget, &depthTarget, &scene );
-
-    DestroyWindow( hWnd );
-
-    return (int)msg.wParam;
+    demoApp.Destroy();
+    return returnCode;
 }
 
 
--- Lighting/PCH.h
+++ Lighting/PCH.h
@@ -6,9 +6,6 @@
 #define NOMINMAX
 // Windows Header Files
 #include <windows.h>
-#include <wrl/client.h>
-#include <wincodec.h>
-#include <wincodecsdk.h>
 
 // C RunTime Header Files
 #include <malloc.h>
@@ -19,8 +16,5 @@
 #include <cassert>
 #include <cmath>
 #include <algorithm>
-
-#include <d2d1.h>
-#include <d2d1helper.h>
 
 #include <DirectXMath.h>
